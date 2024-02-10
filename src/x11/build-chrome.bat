f10c82c14fa96063754d7c354663b8edf1566269
Matthew Whited <matt@whited.us>
2024-02-10 12:46:49 -0500

    add last

--- src/x11/build-chrome.bat
+++ src/x11/build-chrome.bat
@@ -11,4 +11,16 @@ docker run --rm -it ^
 --gpus all ^
 oobdev/google-chrome
 
-docker compose -f docker-compose.google-chrome.yaml up
+REM docker compose -f docker-compose.google-chrome.yaml up
+
+
+docker run --rm -it ^
+-v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix ^
+-v /run/desktop/mnt/host/wslg:/mnt/wslg ^
+-v google-chrome-cache:/root/.cache/google-chrome ^
+-v google-chrome-config:/root/.config/google-chrome ^
+--gpus all ^
+oobdev/google-chrome bash
+
+@REM -v google-chrome-cache:/home/docker/.cache/google-chrome ^
+@REM -v google-chrome-config:/home/docker/.config/google-chrome ^
